e11cc96e33aec65ec03c16afc7359033d01b8f64
kikinF <61199636+kikinF@users.noreply.github.com>
2020-03-12 09:00:29 -0700

    Nueva practica enfocada en diagramas de flujo
    Es un sencillo algoritmo para sumar numeros con su diagrama de fliujo respectivo

---
+++ Practica4.py
@@ -0,0 +1,12 @@
+#El siguiente algoritmo resuleve el problema
+#para realizar la suma de dos numeros decimales
+
+a = 0
+b = 0 
+c = 0
+
+a = float(input("Ingrese el primer numero"))
+b = float(input("Ingrese el segundo valor"))
+c = a + b
+
+print("La suma es: " + str(c))
